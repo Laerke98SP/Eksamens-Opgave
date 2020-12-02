5c0e994ad721a60a7026628e8ab995e4c8ca8662
Lærke Stald Pedersen <laerke20ad@student.cbs.dk>
2020-12-02 16:06:02 +0100

    Started connecting to frontend

--- API/Index.js
+++ API/Index.js
@@ -8,6 +8,7 @@ import userInterest from './routes/inserest.js';
 const app = express();
 const PORT = 5000;
 
+app.use(express.static('../Frontend'))
 app.use(bodyParser.json());
 
 app.use('/user', userRoutes);
